b49108d29f429fb478e8fc4f9fac3f8d27636388
Omar <olr7742@nyu.edu>
2026-01-19 03:18:35 -0800

    refactor

--- teledex/handlers/mujoco_handler.py
+++ teledex/handlers/mujoco_handler.py
@@ -23,8 +23,8 @@ class MujocoHandler:
             data: The MuJoCo data object.
         """
         # Import mujoco only when needed
-        import mujoco
-        self.mujoco = mujoco
+        import teledex.handlers.mujoco_handler as mujoco_handler
+        self.mujoco = mujoco_handler
         
         self.model = model
         self.data = data
