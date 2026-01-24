db57a6a8514d162fd69dbda91d95282645b062b1
Omar <olr7742@nyu.edu>
2026-01-24 01:58:20 -0800

    updated stup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="teledex",
-    version="0.0.1",
+    version="0.0.5",
     description="Phone-based Dexterous Robots teleoperation library",
     packages=find_packages(),
     python_requires=">=3.8",
